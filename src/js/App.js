afaef3f412434146b548a9438bde2d3729f0837a
Bibek <ghim.bibek@gmail.com>
2019-05-24 15:27:18 -0700

    Fix completion page height

--- src/js/App.js
+++ src/js/App.js
@@ -10,7 +10,8 @@ import AboutPage from './components/AboutPage'
 import copy from './copy'
 import { fonts } from './styles'
 
-injectGlobal([`
+injectGlobal([
+  `
     @import url('https://fonts.googleapis.com/css?family=Lora:400,700|Open+Sans:400,700');
 
     body {
@@ -27,64 +28,74 @@ injectGlobal([`
     *, *:before, *:after {
         box-sizing: border-box;
     }
-`])
+`,
+])
 
-const AppWrapper = styled.div`
-`
+const AppWrapper = styled.div``
 
 AppWrapper.displayName = 'AppWrapper'
 
 type PropsType = {}
 
 type StateType = {
-    currentView: 'landing' | 'writing',
-    totalWriteTime: ?number,
-    writingPrompt: ?string
+  currentView: 'landing' | 'writing',
+  totalWriteTime: ?number,
+  writingPrompt: ?string,
 }
 
 class App extends React.Component<PropsType, StateType> {
-    constructor(props: PropsType) {
-        super(props)
-        this.state = {
-            currentView: 'landing',
-            totalWriteTime: null
-        }
-        this.handleStartRequest = this.handleStartRequest.bind(this)
-        this.handleCompletion = this.handleCompletion.bind(this)
+  constructor(props: PropsType) {
+    super(props)
+    this.state = {
+      currentView: 'landing',
+      totalWriteTime: null,
     }
+    this.handleStartRequest = this.handleStartRequest.bind(this)
+    this.handleCompletion = this.handleCompletion.bind(this)
+  }
 
-    handleStartRequest(time: number, wantWritingPrompt: boolean) {
-        this.setState({ 
-            currentView: 'writing',
-            totalWriteTime: time * 60 * 1000,
-            writingPrompt: wantWritingPrompt ? copy.writingPrompts[Math.floor(Math.random() * copy.writingPrompts.length)] : null
-        })
-    }
+  handleStartRequest(time: number, wantWritingPrompt: boolean) {
+    this.setState({
+      currentView: 'writing',
+      totalWriteTime: time * 60 * 1000,
+      writingPrompt: wantWritingPrompt
+        ? copy.writingPrompts[
+            Math.floor(Math.random() * copy.writingPrompts.length)
+          ]
+        : null,
+    })
+  }
 
-    handleCompletion() {
-        this.setState({
-            currentView: 'completion'
-        })
-    }
+  handleCompletion() {
+    this.setState({
+      currentView: 'completion',
+    })
+  }
 
-    render() {
-        const renderer = {
-            landing: () => <LandingView onStartRequest={this.handleStartRequest} />,
-            writing: () => <WritingView totalWriteTime={this.state.totalWriteTime} writingPrompt={this.state.writingPrompt} onCompletion={this.handleCompletion} />,
-            completion: () => <CompletionView />
-        }[this.state.currentView]
+  render() {
+    const renderer = {
+      landing: () => <LandingView onStartRequest={this.handleStartRequest} />,
+      writing: () => (
+        <WritingView
+          totalWriteTime={this.state.totalWriteTime}
+          writingPrompt={this.state.writingPrompt}
+          onCompletion={this.handleCompletion}
+        />
+      ),
+      completion: () => <CompletionView />,
+    }[this.state.currentView]
 
-        return (
-            <AppWrapper>
-                <BrowserRouter>
-                    <Switch>
-                        <Route path='/about' render={() => <AboutPage />} />
-                        <Route exact path='/' render={renderer} />
-                    </Switch>
-                </BrowserRouter>
-            </AppWrapper>
-        )
-    }
+    return (
+      <AppWrapper>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/about" render={() => <AboutPage />} />
+            <Route exact path="/" render={renderer} />
+          </Switch>
+        </BrowserRouter>
+      </AppWrapper>
+    )
+  }
 }
 
 export default App
--- src/js/components/CompletionView.js
+++ src/js/components/CompletionView.js
@@ -7,20 +7,22 @@ import Text from './Text'
 import { colors } from '../styles'
 
 const Wrapper = styled.div`
-    width: 100%;
-    height: 100%;
-    background-color: ${colors.green};
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  width: 100%;
+  height: 100vh;
+  background-color: ${colors.green};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 export default function() {
-    return (
-        <Wrapper>
-            <Title>Great writing session today.</Title>
-            <Text bold center>I hope you were able to get some things off your mind.</Text>
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <Title>Great writing session today.</Title>
+      <Text bold center>
+        I hope you were able to get some things off your mind.
+      </Text>
+    </Wrapper>
+  )
 }
